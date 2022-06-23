c28afac43304b9e21c1aed6fb66d5d3c81667312
sarad096 <saradgyawali096@gmail.com>
2022-06-23 13:31:30 +0545

    Added Particle and typewriter effect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./header.css";
+import Typewriter from "typewriter-effect";
+import Particle from './Particle.jsx'
 import IMG from "../../Assets/logohero.png";
 const Header = () => {
   return (
       <section className="header">
+<Particle />
       <div className="logo">
         <img src={IMG} alt="" />
       </div>
@@ -14,6 +17,20 @@ const Header = () => {
         <li><a href="#">Team</a></li>
         <li><a href="#">Partners</a></li>
       </div>
+ 
+
+
+    <div className="typewriter">
+    <Typewriter
+  options={{
+    strings: ['We are Non-Political', 'We are Technological','We are Non-Profit','We are Community'],
+    autoStart: true,
+    loop: true,
+  }}
+/>
+    </div>
+
+      
       </section>
   );
 };
