6edd258542643b704925b0089029247a5f3fab13
sarad096 <saradgyawali096@gmail.com>
2022-06-23 09:37:55 +0545

    firstcommit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./header.css"
+const Header = () => {
+  return (
+    <div>
+
+<section className='header'>
+
+    
+</section>
+
+    </div>
+  )
+}
+
+export default Header
