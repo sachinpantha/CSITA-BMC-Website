d003cc461314c70e5cbd1a4e336b76275e985937
sarad096 <saradgyawali096@gmail.com>
2022-06-24 08:29:42 +0545

    Modified Flexbox Dimension

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -7,11 +7,7 @@ const Product = ({img,title,link,id}) => {
       <div className="project-info">
           <span className='p-title'>{title}</span>
       </div>
-      <div className="p-browser">
-        <div className="p-circle"></div>
-        <div className="p-circle" style={{'backgroundColor':'yellow'}}></div>
-        <div className="p-circle" style={{'backgroundColor':'green'}}></div>
-      </div>
+  
         <img src={img} alt="" className='p-img' />
     </a>
     </>
