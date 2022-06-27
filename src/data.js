8fce46eb7f45ac79afc399565e6570b950f02247
sarad096 <saradgyawali096@gmail.com>
2022-06-27 11:46:20 +0545

    Updated Events Section And Updated Body Dimensions

--- src/data.js
+++ src/data.js
@@ -1,3 +1,9 @@
+import mocktest from "./Assets/eventimg/mocktest.jpg"
+import frontendworkshop from "./Assets/eventimg/frontendworkshop.jpg"
+import futsal from "./Assets/eventimg/futsal.jpg"
+import graphicdesigning from "./Assets/eventimg/graphicdesigning.jpg"
+import itroadmap from "./Assets/eventimg/itroadmap.jpg"
+import workshopNET from "./Assets/eventimg/workshopNET.jpg"
 export const Links=[
   {
     title:'Home',
@@ -28,31 +34,31 @@ export const products = [
   {
     id: 1,
     title: "Mock Test 2079",
-    img: "https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/271777635_1253484145119238_3805090052641742902_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=iJLkVB3HBnQAX-52l8Z&_nc_ht=scontent.fktm10-1.fna&oh=00_AT-5_9Avs6ERCHgPPSsc47E1BFV6EEAGeT1j0M8zDzAxlA&oe=62BACB7F",
+    img: mocktest,
   },
   {
     id: 2,
     title: "Frontend Workshop",
-    img: "https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/272898222_1324014831400823_9005928097344204733_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=K7f2zOEX7KAAX_M9S5u&_nc_ht=scontent.fktm10-1.fna&oh=00_AT-Uqy1_MXR-ma_aImbTOds3NtiHd4S4wHpIzDiF4e3f9w&oe=62B93837",
+    img: frontendworkshop,
   },
   {
     id: 3,
     title: "Futsal Tournament",
-    img: "https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/277226840_1355402794928693_6156228628291743978_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=d-JJ9pSbESsAX9O8gpI&tn=dLyGCWnCkX5hDukj&_nc_ht=scontent.fktm10-1.fna&oh=00_AT-bvo995j1F23tHGRWQVd_bNM37I9mgw3tRXKLtA2cYgA&oe=62BA8145",
+    img: futsal,
   },
   {
     id: 4,
     title: "Graphic Designing Workshop",
-    img: "https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/277294468_1355684788233827_1504629437497011314_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=du62Ujp53HcAX8OXpjK&_nc_oc=AQmhmoQajE_wOd0Bfs39Q8U0CicyS2LZ_FngbQFElAoUWqAjZ_snq82F-USTjoC3cSI&_nc_ht=scontent.fktm10-1.fna&oh=00_AT9w2tDhgr-hGYL8XuT3-yiSR79QP1QLFdPJbQeW45ozKA&oe=62B961BA",
+    img: graphicdesigning,
   },
   {
     id: 5,
     title: "IT Roadmap Event",
-    img: "https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/284125137_1397753614026944_3103846505998210479_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=nBH2rxRgaTAAX9FhnNg&_nc_oc=AQlWu99xhrwDuAVu-m1YQjZ7HvEramzjKN7F4yj04FxmY8DPp0nZK8aoGGZ7veoPBRg&_nc_ht=scontent.fktm10-1.fna&oh=00_AT8eKoEIVyoxlRJBbQVa7dPjvlqV_Qtd1ySXaR0RlJEI0g&oe=62BA70C8",
+    img: itroadmap,
   },
   {
     id: 6,
     title: ".NET Workshop",
-    img: "https://scontent.fktm10-1.fna.fbcdn.net/v/t1.6435-9/201273162_1174878282981146_6723673219792473022_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=JlYEjsqYtWoAX95lKR_&_nc_ht=scontent.fktm10-1.fna&oh=00_AT8JrudniG6BQ-ppBttLbSDsCyaMQxVZGUfWNAOFP8FIHw&oe=62D95E34",
+    img: workshopNET,
   },
 ];
