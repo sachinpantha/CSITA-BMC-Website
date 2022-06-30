15b1f21e9a40934222c059c69eba5b7fed7ecd92
sarad096 <saradgyawali096@gmail.com>
2022-06-30 17:42:18 +0545

    Aligned Typewriter Position

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<>
+
+
     <App />
-  </React.StrictMode>
+    <MessengerCustomerChat
+    pageId="2032410933676554"
+    appId="590338082605549"
+    htmlRef="<REF_STRING>"
+  />,
+  </>
+
 );
